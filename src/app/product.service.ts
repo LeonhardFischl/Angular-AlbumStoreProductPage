2ad5addda0e62020dfab95fc1c89c3fd5cdc8ba9
LeonhardFischl <leo-fischl@gmx.de>
2020-09-27 15:40:30 +0200

    Completed module

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -3,12 +3,15 @@ import { Http, Response } from '@angular/http';
 import 'rxjs/add/operator/map';
 import { Observable } from 'rxjs/Observable';
 import { Album } from './album';
+import { Product } from './product'
 
 @Injectable()
 export class ProductService {
 
 private _albumUrl = '../assets/album.json';
+private _productsUrl = '../assets/products.json'
 private album: Album;
+
 
   constructor(private _http: Http) { }
 
@@ -16,4 +19,7 @@ private album: Album;
     return this._http.get(this._albumUrl).map((response) => <Album>response.json());
   }
 
+  getProducts(): Observable<Product[]> {
+    return this._http.get(this._productsUrl).map((response) => <Product[]>response.json());
+  }
 }
